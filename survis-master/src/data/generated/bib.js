594bee40b866775b596d63429d8a870be5a57ccc
Junzhuo888 <psxjj9@nottingham.ac.uk>
2026-05-10 19:55:08 +0100

    Add files via upload

---
+++ survis-master/src/data/generated/bib.js
@@ -0,0 +1,134 @@
+﻿const generatedBibEntries = {
+  "abhari2023mitigating": {
+    "type": "inproceedings",
+    "author": "Abhari, J. and Ashok, A.",
+    "title": "Mitigating Racial Biases for Machine Learning Based Skin Cancer Detection",
+    "booktitle": "Proceedings of the Twenty-fourth International Symposium on Theory, Algorithmic Foundations, and Protocol Design for Mobile Networks and Mobile Computing",
+    "pages": "556--561",
+    "year": "2023",
+    "publisher": "Association for Computing Machinery",
+    "address": "New York, NY, USA",
+    "doi": "10.1145/3565287.3617639",
+    "url": "https://doi.org/10.1145/3565287.3617639",
+    "keywords": "fairness, racial bias, machine learning, skin cancer detection"
+  },
+
+  "degoma2026machine": {
+    "type": "inproceedings",
+    "author": "de Goma, J. and Esteller, C. M. and Fernandez, L. and Macalintal, E. B. and Villaluz, A.",
+    "title": "Machine Learning-Based Skin Lesion Classification with Feature Importance Analysis using SVM for Dermoscopy Images in the Philippines",
+    "booktitle": "Proceedings of the 2025 7th World Symposium on Software Engineering",
+    "pages": "209--215",
+    "year": "2026",
+    "publisher": "Association for Computing Machinery",
+    "address": "New York, NY, USA",
+    "doi": "10.1145/3779657.3779688",
+    "url": "https://doi.org/10.1145/3779657.3779688",
+    "keywords": "SVM, feature importance, dermoscopy images, skin lesion classification"
+  },
+
+  "esteva2017dermatologist": {
+    "type": "article",
+    "author": "Esteva, A. and Kuprel, B. and Novoa, R. A. and Ko, J. and Swetter, S. M. and Blau, H. M. and Thrun, S.",
+    "title": "Dermatologist-level classification of skin cancer with deep neural networks",
+    "journal": "Nature",
+    "volume": "542",
+    "pages": "115--118",
+    "year": "2017",
+    "doi": "10.1038/nature21056",
+    "url": "https://doi.org/10.1038/nature21056",
+    "keywords": "deep learning, neural networks, skin cancer classification, clinical image analysis"
+  },
+
+  "ghosh2024melanoma": {
+    "type": "article",
+    "author": "Ghosh, S. and Dhar, S. and Yoddha, R. and Kumar, S. and Thakur, A. K. and Jana, N. D.",
+    "title": "Melanoma skin cancer detection using ensemble of machine learning models considering deep feature embeddings",
+    "journal": "Procedia Computer Science",
+    "volume": "235",
+    "pages": "3007--3015",
+    "year": "2024",
+    "doi": "10.1016/j.procs.2024.04.284",
+    "url": "https://doi.org/10.1016/j.procs.2024.04.284",
+    "keywords": "melanoma, ensemble learning, deep feature embeddings, skin cancer detection"
+  },
+
+  "gouda2022detection": {
+    "type": "article",
+    "author": "Gouda, W. and Sama, N. U. and Al-Waakid, G. and Humayun, M. and Jhanjhi, N. Z.",
+    "title": "Detection of Skin Cancer Based on Skin Lesion Images Using Deep Learning",
+    "journal": "Healthcare",
+    "volume": "10",
+    "number": "7",
+    "pages": "1183",
+    "year": "2022",
+    "doi": "10.3390/healthcare10071183",
+    "url": "https://doi.org/10.3390/healthcare10071183",
+    "keywords": "deep learning, skin lesion images, skin cancer detection"
+  },
+
+  "rangaswamy2025skin": {
+    "type": "article",
+    "author": "Rangaswamy, S. and Tantry, S. S. and Lal, T. S.",
+    "title": "Skin Disease Classification Using Deep Learning",
+    "journal": "National Academy Science Letters",
+    "volume": "48",
+    "pages": "585--588",
+    "year": "2025",
+    "doi": "10.1007/s40009-024-01523-z",
+    "url": "https://doi.org/10.1007/s40009-024-01523-z",
+    "keywords": "deep learning, skin disease classification, medical image classification"
+  },
+
+  "ranjankumar2025deep": {
+    "type": "article",
+    "author": "Ranjan Kumar, H. S. and Gireesh Babu, C. N. and Vijay, C. P.",
+    "title": "Deep Learning-Based Automated Classification of Skin Lesions Using CNN and Computer Vision",
+    "journal": "SN Computer Science",
+    "volume": "6",
+    "pages": "846",
+    "year": "2025",
+    "doi": "10.1007/s42979-025-04375-6",
+    "url": "https://doi.org/10.1007/s42979-025-04375-6",
+    "keywords": "CNN, computer vision, skin lesion classification, deep learning"
+  },
+
+  "sara2025automated": {
+    "type": "article",
+    "author": "Sara, U. and Das, U. K. and Sikder, J. and Chowdhury, S. R.",
+    "title": "Automated skin Cancer classification and detection using convolutional neural networks and dermoscopy images",
+    "journal": "Procedia Computer Science",
+    "volume": "252",
+    "pages": "108--117",
+    "year": "2025",
+    "doi": "10.1016/j.procs.2024.12.012",
+    "url": "https://doi.org/10.1016/j.procs.2024.12.012",
+    "keywords": "CNN, dermoscopy images, skin cancer classification, automated detection"
+  },
+
+  "yadav2024systematic": {
+    "type": "article",
+    "author": "Yadav, R. and Bhat, A.",
+    "title": "A systematic literature survey on skin disease detection and classification using machine learning and deep learning",
+    "journal": "Multimedia Tools and Applications",
+    "volume": "83",
+    "pages": "78093--78124",
+    "year": "2024",
+    "doi": "10.1007/s11042-024-18119-w",
+    "url": "https://doi.org/10.1007/s11042-024-18119-w",
+    "keywords": "survey, skin disease detection, machine learning, deep learning"
+  },
+
+  "yang2025advancements": {
+    "type": "article",
+    "author": "Yang, G. and Luo, S. and Greer, P.",
+    "title": "Advancements in skin cancer classification: a review of machine learning techniques in clinical image analysis",
+    "journal": "Multimedia Tools and Applications",
+    "volume": "84",
+    "pages": "9837--9864",
+    "year": "2025",
+    "doi": "10.1007/s11042-024-19298-2",
+    "url": "https://doi.org/10.1007/s11042-024-19298-2",
+    "keywords": "review, skin cancer classification, machine learning, clinical image analysis"
+  }
+};
